9a73910703c98609bd4e15f95bfb78748863021c
Liem Pham <liem@healthtech1.uk>
2025-04-30 22:03:59 +0100

    feat: move header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
-import { Header, Hero, HowItWorks, Footer } from '@/_components';
+import { Hero, HowItWorks, Footer } from '@/_components'; 
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-gradient-to-b from-gray-50 to-white dark:from-gray-900 dark:to-gray-800">
-      <Header />
       <Hero />
       <HowItWorks />
       <Footer />
